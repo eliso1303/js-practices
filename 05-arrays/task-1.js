21a6d3e54b52d1bb2235386abc2252828816a32e
Elisabed Papashvili <oboe1303@gmail.com>
2019-05-30 18:45:35 +0400

    feat(05-arrays): Finish Task-1

---
+++ 05-arrays/task-1.js
@@ -0,0 +1,15 @@
+const array = [1, 2, 3];
+
+function forEach(arr, fun) {
+    if (arr && Array.isArray(arr) && fun && typeof fun === "function") {
+        for (var i = 0; i < arr.length; i++) {
+            fun(arr[i], i, arr);
+        }
+    } else {
+        throw new Error("Please enter ")
+    }
+}
+
+forEach(array, function (item, i, arr) {
+    console.log(i + ': ' + arr[i] + ' (array: ' + arr + ')');
+});
