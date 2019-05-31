887a536406f3cbd940b5b2ea57b23e9aea29d655
Elisabed Papashvili <oboe1303@gmail.com>
2019-05-31 20:50:05 +0400

    feat(05-arrays): Finish Task-5, Task-6

--- 05-arrays/task-5.js
+++ 05-arrays/task-5.js
@@ -3,14 +3,19 @@ const acc = 0;
 
 function reduce(arr, fun, start) {
     if (arr.length && Array.isArray(arr) && fun && typeof fun === "function" && typeof start === 'string' || typeof start === 'number') {
+        let value = start; 
+
         for (let i = 0; i < arr.length; i++) {
-            console.log(fun(start, arr[i], i, arr));
+            value = fun(value, arr[i], i, arr);
         }
+        return value;
     } else {
         throw new Error("Please enter correct arguments");
     }
 }
 
-reduce(array, function (acc, item, i, arr) {
+let result = reduce(array, function (acc, item, i, arr) {
     return acc + item;
-}, 0);
+}, acc);
+
+console.log(result);
---
+++ 05-arrays/task-6.js
@@ -0,0 +1,21 @@
+const array = [1, 2, 3];
+const acc = 0;
+
+function reduce(arr, fun, start) {
+    if (arr.length && Array.isArray(arr) && fun && typeof fun === "function" && typeof start === 'string' || typeof start === 'number') {
+        let value = start; 
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            value = fun(value, arr[i], i, arr);
+        }
+        return value;
+    } else {
+        throw new Error("Please enter correct arguments");
+    }
+}
+
+let result = reduce(array, function (acc, item, i, arr) {
+    return acc + item;
+}, 'acc');
+
+console.log(result);
