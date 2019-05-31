8f5e9c9fb9b15e8d1c986a0a7f8d25874112e5d9
Elisabed Papashvili <oboe1303@gmail.com>
2019-05-31 20:32:28 +0400

    fix(05-arrays): Fix error conditions

--- 05-arrays/task-8.js
+++ 05-arrays/task-8.js
@@ -1,11 +1,15 @@
 const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
 
 function f(arr) {
-    if (!(arr === undefined || arr.length == 0)) {
+    if (Array.isArray(arr) && arr.length !== 0) {
         let sum = 0;
         let newArr = arr.flat(20);
         for (let i = 0; i < newArr.length; i++) {
-            sum += newArr[i];
+            if (typeof newArr[i] === 'number') {
+                sum += newArr[i];
+            } else {
+                throw new Error('not a number');
+            }
         }
         console.log(sum);
         return sum;
