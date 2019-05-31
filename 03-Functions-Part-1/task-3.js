c73bd28afbf3fa62b60d05e2288b83fd43091f5c
Elisabed Papashvili <oboe1303@gmail.com>
2019-05-31 10:06:44 +0400

    feat(03-Functions-Part-1): Finish Task-8 and fix returns

--- 03-Functions-Part-1/task-3.js
+++ 03-Functions-Part-1/task-3.js
@@ -7,8 +7,8 @@ var f = function() {
             throw new Error('all parameters type should be a Number');
         }
     }
-    console.log(result);
+    return result;
 };
 
 f(9,3,2); // 3
-f('s',9,3) // Error: all parameters type should be a Number
+f('s',9,3); // Error: all parameters type should be a Number
