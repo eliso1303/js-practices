0a889a8ac220143374fc60dc3013694824a1473d
Elisabed Papashvili <oboe1303@gmail.com>
2019-05-31 11:40:13 +0400

    fix(05-arrays): Fix Task-3

--- 05-arrays/task-3.js
+++ 05-arrays/task-3.js
@@ -1,30 +1,20 @@
 const array = [1,2,3];
 
-
 function every(arr, fun) {
-    if (arr && Array.isArray(arr) && fun && typeof fun === "function") {
+    if (arr.length && Array.isArray(arr) && fun && typeof fun === "function") {
+        let value = true;
         for (var i = 0; i < arr.length; i++) {
-            if(fun(arr[i], i, arr)) {
-                console.log(true);
-                return true;
-            } else {
-                console.log(false);
-                return false;
+            if(!fun(arr[i], i, arr)){
+                value = false;
             }
         }
+        console.log(value);
+        return value;
     } else {
-        throw new Error("Please enter arguments");
+        throw new Error("Please enter correct arguments");
     }
 }
 
 every(array, function(item, i, arr) {
-    item > 1;
+    return item > 0;
 });
-
-// var arr = [1, -1, 2, -2, 3];
-
-// var status = arr.every(function(n){
-//   return n > 0;
-// });
-
-// console.log(status);
