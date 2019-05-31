753481e90e9bdf3bbb66048a59bc605dbb10e5ba
Elisabed Papashvili <oboe1303@gmail.com>
2019-05-31 13:38:05 +0400

    feat(05-arrays): Finish Task-7, Task-8

---
+++ 05-arrays/task-5.js
@@ -0,0 +1,16 @@
+const array = [1, 2, 3];
+const acc = 0;
+
+function reduce(arr, fun, start) {
+    if (arr.length && Array.isArray(arr) && fun && typeof fun === "function" && typeof start === 'string' || typeof start === 'number') {
+        for (var i = 0; i < arr.length; i++) {
+            console.log(fun(start, arr[i], i, arr));
+        }
+    } else {
+        throw new Error("Please enter correct arguments");
+    }
+}
+
+reduce(array, function (acc, item, i, arr) {
+    return acc + item;
+}, 0);
---
+++ 05-arrays/task-8.js
@@ -0,0 +1,18 @@
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+
+function f(arr) {
+    if (!(arr === undefined || arr.length == 0)) {
+        let sum = 0;
+        var newArr = arr.flat(20);
+        for (var i = 0; i < newArr.length; i++) {
+            sum += newArr[i];
+        }
+        console.log(sum);
+        return sum;
+    } else {
+        console.log(0);
+        return 0;
+    }
+}
+
+f(arr); // 12  
