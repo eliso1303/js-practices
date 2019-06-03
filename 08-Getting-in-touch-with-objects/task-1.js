6c4293bd1e1303fc8c74859d49c2fe1b89ee9337
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-03 21:40:01 +0400

    feat(08-Getting-in-touch-with-objects): Finish Task-1

--- 08-Getting-in-touch-with-objects/task-1.js
+++ 08-Getting-in-touch-with-objects/task-1.js
@@ -1,3 +1,17 @@
 const person = {
-    
+    saleryNum: 1500
 };
+
+Object.defineProperty(person, 'salary', {
+    get() {
+        let d = new Date();
+        let monthDays = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
+        if (d.getDate() < (monthDays - 20)) {
+            return this.saleryNum + ' Good salery';
+        } else {
+            return this.saleryNum + ' Bad salery';
+        }
+    },
+});
+
+console.log(person.salary);
