f6cba22860063d741f09b1a34ecec543df1c734f
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-05 22:05:28 +0400

    fix(10-Object-Oriented-Programming-Part-1): Fix task 1
    n

--- 10-Object-Oriented-Programming-Part-1/task-1.js
+++ 10-Object-Oriented-Programming-Part-1/task-1.js
@@ -1,26 +1,30 @@
 function CoffeeMachine(power) {
     let timerId;
 
-	this.waterAmount = 0;
-	let WATER_HEAT_CAPACITY = 4200;
-	let self = this;
-	
+    this.waterAmount = 0;
+    const WATER_HEAT_CAPACITY = 4200;
+    let self = this;
+
     function getBoilTime() {
-		return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-	}
-	
+        return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    }
+
     function onReady() {
-		console.log('Coffee is ready');
-	}
-	
-    this.run = function() {
-		timerId = setTimeout(onReady, getBoilTime());
+        console.log('Coffee is ready');
+    }
+
+    this.run = function () {
+        timerId = setTimeout(onReady, getBoilTime());
     };
-    
-    this.stop = function() {
-        clearTimeout(timerId);
-        timerId = void 0;
-        console.log('coffee isn\'t ready');
+
+    this.stop = function () {
+        if (timerId) {
+            clearTimeout(timerId);
+            timerId = void 0;
+            console.log('coffee isn\'t ready');
+        } else {
+            throw new Error("coffee making don't started");
+        }
     }
 }
 
