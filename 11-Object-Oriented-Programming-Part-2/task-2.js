ba6c38a983ce80667aced328744d04fa681a7dd4
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-07 21:23:42 +0400

    fix(11-Object-Oriented-Programming-Part-2): Fix  - getTimeToBoil to prototype

--- 11-Object-Oriented-Programming-Part-2/task-2.js
+++ 11-Object-Oriented-Programming-Part-2/task-2.js
@@ -1,22 +1,24 @@
 function CoffeeMachine(power) {
-    let waterAmount = 0;
-    let WATER_HEAT_CAPACITY = 4200;
+    this.waterAmount = 0;
+    this.WATER_HEAT_CAPACITY = 4200;
+    this.power = power
+}
 
-    this.getTimeToBoil = function() {
-        return this.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-    }
+CoffeeMachine.prototype.getTimeToBoil = function () {
+    return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power
 }
 
 CoffeeMachine.prototype.run = function () {
     setTimeout(function () {
         console.log('Coffee is ready!');
     }, this.getTimeToBoil());
-}
+};
 
 CoffeeMachine.prototype.setWaterAmount = function (amount) {
-    waterAmount = amount;
-}
+    this.waterAmount = amount;
+};
 
-var coffeeMachine = new CoffeeMachine(10000);
+let coffeeMachine = new CoffeeMachine(10000);
 coffeeMachine.setWaterAmount(50);
 coffeeMachine.run();
+
