ff783f0f4608590debfea065d5f6376be39e3067
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-10 21:25:15 +0400

    fix(12-Introducing-Javascript-Classes): Fix isDomain and isDate functions

--- 12-Introducing-Javascript-Classes/task-5.js
+++ 12-Introducing-Javascript-Classes/task-5.js
@@ -9,17 +9,21 @@ class Validator {
             return false;
         }
     }
-    
+
     isDomain(domain) {
-        if (domain.lastIndexOf('.') < domain.length - 1) {
-            return true;
-        } else {
-            return false;
+        let domainArr = ['.com', '.ge', '.org', '.net'], domainTrue;
+        for (let i = 0; i < domainArr.length; i++) {
+            if (domain.indexOf(domainArr[i]) !== -1) {
+                domainTrue = true;
+            } else {
+                domainTrue = false;
+            }
         }
+        return domainTrue;
     }
 
     isDate(date) {
-        if (date.indexOf('.') === 2 && date.lastIndexOf('.') === 5) {
+        if (date.indexOf('.') === 2 && date.lastIndexOf('.') === 5 && date.substring(0, 2) <= 31 && date.substring(3, 5) <= 12) {
             return true;
         } else {
             return false;
@@ -38,5 +42,5 @@ var validator = new Validator();
 
 console.log(validator.isEmail('jshtml@mail.ru'));
 console.log(validator.isDomain('jshtml.net'));
-console.log(validator.isDate('12.05.2020'));
+console.log(validator.isDate('31.12.2020'));
 console.log(validator.isPhone('+375 (29) 817-68-92')); // it can be format of your country
