e8a8555ee6a7207ec85458daf9c579b29e9a402f
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-14 22:09:00 +0400

    fix(15-Sets-and-Maps): Fix task-2

--- 15-Sets-and-Maps/task-2.js
+++ 15-Sets-and-Maps/task-2.js
@@ -4,23 +4,17 @@ class DB {
     }
 
     create(object) {
-        if (object || typeof object === "object") {
-            if (object.name || typeof object.name === 'string' || object.age || typeof object.age === 'number' || object.country || typeof object.country === 'string' || object.salary || typeof object.salary === 'number') {
-                let _id = new Date().getUTCMilliseconds().toString();
-                this.db.set(_id, object);
-                return _id;
-            } else {
-                throw new Error('Invalid property');
-            }
-        } else {
-            throw new Error('Must be an object');
-        }
+        this.validateFields(object);
+        let _id = new Date().getUTCMilliseconds().toString();
+        this.db.set(_id, object);
+        return _id;
     }
 
     read(userId) {
         if (userId || typeof userId === 'string') {
             if (this.db.has(userId)) {
                 this.db.get(userId).id = userId;
+                return this.db.get(userId);
             } else {
                 return null;
             }
@@ -29,9 +23,10 @@ class DB {
         }
     }
 
-    readAll(value) {
-        if (!value) {
-            return Array.from(this.db.values());
+    readAll() {
+        if (arguments.length === 0) {
+            let arr = Array.from(this.db.values());
+            return arr;
         } else {
             throw new Error('Please, don\'t set parameter');
         }
@@ -44,6 +39,14 @@ class DB {
 
         if (!changes || typeof changes !== 'object') {
             throw new Error('invalid entry');
+        }
+
+        if(changes.name && typeof changes.name !== 'string' || changes.country && typeof changes.country !== 'string') {
+            throw new Error('Property required to be string');
+        }
+
+        if (changes.age && typeof changes.age !== 'number' || changes.salary && typeof changes.salary !== 'number') {
+            throw new Error('Property required to be number');
         }
 
         if (this.db.has(id)) {
@@ -68,26 +71,57 @@ class DB {
         }
     }
 
+    validateFields(object) {
+        if (typeof object !== 'object') {
+            throw new Error('Must be an object');
+        }
+
+        const properties = ['name', 'age', 'country', 'salary'];
+
+        for (let i = 0; i < properties.length; i++) {
+            if (!object.hasOwnProperty(properties[i])) {
+                throw new Error(property + ' is required');
+            }
+        }
+
+        if (typeof object.name !== 'string' || typeof object.country !== 'string') {
+            throw new Error('Property required to be string');
+        }
+        if (typeof object.age !== 'number' || typeof object.salary !== 'number') {
+            if (typeof object.age === 'object' || typeof object.salary === 'object') {
+                var arr = [object.age.min, object.age.max, object.salary.min, object.salary.max];
+                for (let i = 0; i < arr.length; i++) {
+                    if (arr[i]) {
+                        if (typeof arr[i] !== 'number') {
+                            throw new Error('Property required to be number');
+                        }
+                    } else {
+                        throw new Error('Property required to be number ');
+                    }
+                }
+            } else {
+                throw new Error('Property required to be number');
+            }
+        }
+    }
+
     find(query) {
         let userArr = [];
-        if (query || typeof query === "object") {
-            userArr = this.readAll().filter(user => {
-                return (
-                    user.name === query.name &&
-                    user.country === query.country &&
-                    ((query.age.min && query.age.max) ? (user.age >= query.age.min && user.age <= query.age.max) : 
-                    query.age.min ? (user.age >= query.age.min) : 
-                    query.age.max ? (user.age <= query.age.max) : false) &&
+        this.validateFields(query);
+        userArr = this.readAll().filter(user => {
+            return (
+                user.name === query.name &&
+                user.country === query.country &&
+                ((query.age.min && query.age.max) ? (user.age >= query.age.min && user.age <= query.age.max) :
+                    query.age.min ? (user.age >= query.age.min) :
+                        query.age.max ? (user.age <= query.age.max) : false) &&
 
-                    ((query.salary.min && query.salary.max) ? (user.salary >= query.salary.min && user.salary <= query.salary.max) : 
-                    query.salary.min ? (user.salary >= query.salary.min) : 
-                    query.salary.max ? (user.salary <= query.salary.max) : false)
-                );
-            });
-            return userArr;
-        } else {
-            throw new Error("Query is not valid!");
-        }
+                ((query.salary.min && query.salary.max) ? (user.salary >= query.salary.min && user.salary <= query.salary.max) :
+                    query.salary.min ? (user.salary >= query.salary.min) :
+                        query.salary.max ? (user.salary <= query.salary.max) : false)
+            );
+        });
+        return userArr;
     }
 }
 
@@ -106,7 +140,7 @@ const query = {
     name: 'Pitter',
     country: 'georgia',
     age: {
-        min: 21
+        min: 0
     },
     salary: {
         min: 500,
