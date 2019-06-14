aa4654ee19d72dbd0bd25ee78313eef88d5a425a
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-14 13:35:24 +0400

    feat(16-Promises): Task-4 in progress

--- 16-Promises/task-4.js
+++ 16-Promises/task-4.js
@@ -1,26 +1,38 @@
-const customers = [
-    {
-        id: 'A1',
-        name: 'Oliver',
-        verified: true
-    },
-    {
-        id: 'A2',
-        name: 'alex'
-    }
-];
+const get = require('fetch').fetchUrl;
+const url = 'https://lab.lectrum.io/geo/api/countries';
 
-const countries = [
-    {
-        id: 'A1',
-        country: 'usa'
-    },
-    {
-        id: 'A2',
-        country: 'poland'
+class countries {
+    constructor(url){
+        if(typeof url !== 'string'){
+            throw new Error("link must be a string");
+        } 
     }
-];
 
-getCustomers(customers, countries)
-    .then((customers) => console.log(customers))
-    .catch(error => console.log(error))
+    send(size) {
+        if(typeof size === "number"){
+            return new Promise((resolve, reject) => {
+                get(size, (error, meta, body) => {
+                    const { data } = JSON.parse(body);
+                    if (meta.status == 200) {
+                        resolve(data);
+                    }else{
+                        reject('We have error, status code: '+ meta.status);
+                    }
+                });
+            });
+        }else{
+            throw new Error ("size must be a number");
+        }
+    } 
+}
+
+const countries = new Countries(url);
+
+(async() => {
+    try {
+        const data = await countries.send(2);
+        console.log(data); // array of countries
+    } catch (error) {
+        console.log(error);
+    }
+})();
