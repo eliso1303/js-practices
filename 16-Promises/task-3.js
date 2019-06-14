573ba65742bcdd9147a6ba4f2f36cfb781180a57
Elisabed Papashvili <oboe1303@gmail.com>
2019-06-14 11:14:50 +0400

    feat(16-Promises): Finish Task-3

--- 16-Promises/task-3.js
+++ 16-Promises/task-3.js
@@ -1,11 +1,18 @@
-const send = (url) => {
+const get = require('fetch').fetchUrl;
+const url = 'https://lab.lectrum.io/geo/api/countries?size=2';
+
+const send = url => {
     return new Promise((resolve, reject) => {
-        
+        get(url, (error, meta, body) => {
+            const { data } = JSON.parse(body);
+            if (meta.status == 200) {
+	            resolve(data);
+            }else{
+                reject('We have error, status code: '+ meta.status);
+            }
+        });
     });
 };
-
-const get = require('fetch');
-const url = 'https://lab.lectrum.io/geo/api/countries?size=2';
 
 send(url)
     .then(data => {
